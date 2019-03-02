a096def129cc4772c68cc31aa752803edf8e101f
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-03-02 16:02:51 +0530

    Added re routing

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -6,31 +6,52 @@ import {
   GoogleMap,
   DirectionsRenderer
 } from "react-google-maps";
-
+let waypoints;
 export default class OptimalMap extends Component {
   state = {
     directions: null
   };
-
-  componentDidMount() {
+  updateMap = waypoints => {
     const directionsService = new google.maps.DirectionsService();
-
     const origin = { lat: 28.7041, lng: 77.1025 };
     const destination = { lat: 30.7333, lng: 76.7794 };
-
     directionsService.route(
       {
         origin: origin,
         destination: destination,
         travelMode: google.maps.TravelMode.DRIVING,
-        waypoints: [
-          {
-            location: new google.maps.LatLng(30.3782, 76.7767)
-          },
-          {
-            location: new google.maps.LatLng(29.0588, 76.0856)
-          }
-        ]
+        waypoints: waypoints
+      },
+      (result, status) => {
+        if (status === google.maps.DirectionsStatus.OK) {
+          this.setState({
+            directions: result
+          });
+        } else {
+          console.error(`error fetching directions ${result}`);
+        }
+      }
+    );
+  };
+  componentDidMount() {
+    const directionsService = new google.maps.DirectionsService();
+
+    const origin = { lat: 28.7041, lng: 77.1025 };
+    const destination = { lat: 30.7333, lng: 76.7794 };
+    waypoints = [
+      {
+        location: new google.maps.LatLng(30.3782, 76.7767)
+      },
+      {
+        location: new google.maps.LatLng(29.0588, 76.0856)
+      }
+    ];
+    directionsService.route(
+      {
+        origin: origin,
+        destination: destination,
+        travelMode: google.maps.TravelMode.DRIVING,
+        waypoints: waypoints
       },
       (result, status) => {
         if (status === google.maps.DirectionsStatus.OK) {
@@ -44,9 +65,22 @@ export default class OptimalMap extends Component {
     );
   }
 
+  handleClick = e => {
+    let lat = e.latLng.lat();
+    let lng = e.latLng.lng();
+    waypoints.push({
+      location: new google.maps.LatLng(lat, lng)
+    });
+    this.updateMap(waypoints);
+  };
+
   render() {
     const GoogleMapExample = withGoogleMap(props => (
-      <GoogleMap defaultCenter={{ lat: 28.7041, lng: 77.1025 }} defaultZoom={8}>
+      <GoogleMap
+        defaultCenter={{ lat: 28.7041, lng: 77.1025 }}
+        defaultZoom={8}
+        onClick={this.handleClick}
+      >
         <DirectionsRenderer directions={this.state.directions} />
       </GoogleMap>
     ));
