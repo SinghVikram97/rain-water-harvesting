7815de378e1e8e24c9577fd105aa8bb1dbd871d0
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-03-03 14:08:40 +0530

    All working

--- src/components/ViewTankRoute.js
+++ src/components/ViewTankRoute.js
@@ -1,16 +1,11 @@
 /*global google*/
 import React, { Component } from "react";
-import "./Advanced.css";
-import {
-  withGoogleMap,
-  withScriptjs,
-  GoogleMap,
-  DirectionsRenderer
-} from "react-google-maps";
-let waypoints;
+import { withGoogleMap, GoogleMap, Polyline, Marker } from "react-google-maps";
 let data = [
   {
     id: 1,
+    lat: 28.7208,
+    lng: 77.1072,
     houses: [
       {
         lat: 28.4595,
@@ -19,11 +14,21 @@ let data = [
       {
         lat: 28.5355,
         lng: 77.391
+      },
+      {
+        lat: 28.4089,
+        lng: 77.3178
+      },
+      {
+        lat: 28.504,
+        lng: 77.3018
       }
     ]
   },
   {
     id: 2,
+    lat: 28.4595,
+    lng: 77.0266,
     houses: [
       {
         lat: 28.7041,
@@ -32,6 +37,18 @@ let data = [
       {
         lat: 28.5355,
         lng: 77.391
+      },
+      {
+        lat: 28.7495,
+        lng: 77.0565
+      },
+      {
+        lat: 28.5503,
+        lng: 77.2502
+      },
+      {
+        lat: 28.5428,
+        lng: 77.2395
       }
     ]
   }
@@ -39,108 +56,99 @@ let data = [
 export default class OptimalMap extends Component {
   state = {
     directions: null,
-    startLat: 28.7041,
-    startLng: 77.1025,
-    endLat: 30.7333,
-    endLng: 76.7794,
-    tankId: 1
-  };
-  updateMap = waypoints => {
-    let latStart = Number(this.state.startLat);
-    let lngStart = Number(this.state.startLng);
-
-    let latEnd = Number(this.state.endLat);
-    let lngEnd = Number(this.state.endLng);
-
-    const directionsService = new google.maps.DirectionsService();
-
-    const origin = { lat: latStart, lng: lngStart };
-    const destination = { lat: latEnd, lng: lngEnd };
-    directionsService.route(
-      {
-        origin: origin,
-        destination: destination,
-        travelMode: google.maps.TravelMode.WALKING,
-        waypoints: waypoints
-      },
-      (result, status) => {
-        if (status === google.maps.DirectionsStatus.OK) {
-          this.setState({
-            directions: result
-          });
-        } else {
-          console.error(`error fetching directions ${result}`);
+    tankId: 1,
+    markerLat: 28.7208,
+    markerLng: 77.1072,
+    networks: [
+      [{ lat: 28.7208, lng: 77.1072 }, { lat: 28.5677, lng: 77.2433 }],
+      [
+        {
+          lat: 28.7208,
+          lng: 77.1072
+        },
+        {
+          lat: 28.5355,
+          lng: 77.391
         }
-      }
-    );
-  };
-  componentDidMount() {
-    const directionsService = new google.maps.DirectionsService();
-
-    const origin = { lat: this.state.startLat, lng: this.state.startLng };
-    const destination = { lat: this.state.endLat, lng: this.state.endLng };
-    waypoints = [
-      // {
-      //   location: new google.maps.LatLng(30.3782, 76.7767)
-      // },
-      // {
-      //   location: new google.maps.LatLng(29.0588, 76.0856)
-      // }
-    ];
-    directionsService.route(
-      {
-        origin: origin,
-        destination: destination,
-        travelMode: google.maps.TravelMode.WALKING,
-        waypoints: waypoints
-      },
-      (result, status) => {
-        if (status === google.maps.DirectionsStatus.OK) {
-          this.setState({
-            directions: result
-          });
-        } else {
-          console.error(`error fetching directions ${result}`);
+      ],
+      [
+        {
+          lat: 28.7208,
+          lng: 77.1072
+        },
+        {
+          lat: 28.8955,
+          lng: 76.6066
         }
-      }
-    );
-  }
-
+      ]
+    ]
+  };
   saveId = e => {
     this.setState({ tankId: e.target.value });
   };
-
   handleBtnClick = () => {
+    let dummyNetwork = [];
     let id = Number(this.state.tankId);
+    let index;
     for (let i = 0; i < data.length; i++) {
       if (data[i].id === id) {
-        let startLat = data[i].houses[0].lat;
-        let startLng = data[i].houses[0].lng;
-        let endLat = data[i].houses[1].lat;
-        let endLng = data[i].houses[1].lng;
-        this.setState(
-          {
-            startLat,
-            startLng,
-            endLat,
-            endLng
-          },
-          () => {
-            this.updateMap(waypoints);
-          }
-        );
+        index = i;
+        break;
       }
     }
-  };
+    let housesArray = data[index].houses;
 
+    for (let i = 0; i < housesArray.length; i++) {
+      dummyNetwork.push([
+        {
+          lat: data[index].lat,
+          lng: data[index].lng
+        },
+        {
+          lat: housesArray[i].lat,
+          lng: housesArray[i].lng
+        }
+      ]);
+    }
+
+    console.log(dummyNetwork);
+    this.setState({
+      networks: dummyNetwork
+    });
+
+    this.setState({
+      markerLat: data[index].lat
+    });
+
+    this.setState({
+      markerLng: data[index].lng
+    });
+  };
   render() {
+    const networks = this.state.networks;
+
     const GoogleMapExample = withGoogleMap(props => (
       <GoogleMap
         defaultCenter={{ lat: 28.7041, lng: 77.1025 }}
-        defaultZoom={8}
+        defaultZoom={12}
         onClick={this.handleClick}
       >
-        <DirectionsRenderer directions={this.state.directions} />
+        <Marker
+          position={{ lat: this.state.markerLat, lng: this.state.markerLng }}
+        />
+        {networks.map(pathCoordinate => {
+          return (
+            <Polyline
+              path={pathCoordinate}
+              geodesic={true}
+              options={{
+                strokeColor: "red",
+                strokeOpacity: 0.75,
+                strokeWeight: 5
+              }}
+            />
+          );
+        })}
       </GoogleMap>
     ));
 
@@ -151,8 +159,11 @@ export default class OptimalMap extends Component {
           style={{ textAlign: "center", paddingLeft: "5%" }}
         >
           <div className="col-3 pl4 offset-3" style={{ textAlign: "center" }}>
-            <h3 style={{ display: "inline-block", fontFamily: "monospace" }}>
-              Tank ID:
+            <h3
+              style={{ display: "inline-block", fontFamily: "monospace" }}
+              className="mr3"
+            >
+              Start:
             </h3>
             <input type="text" onChange={this.saveId} />
           </div>
