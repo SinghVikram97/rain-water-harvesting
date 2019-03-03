0ccdc13efce6d4ac36094435dedb8c8d091dc5e7
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-03-03 14:57:34 +0530

    Added form

--- public/script.js
+++ public/script.js
@@ -3,15 +3,15 @@ window.executeHome = () => {
   var myChart = new Chart(ctx, {
     type: "line",
     data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      labels: ["1", "2", "3", "4", "5", "6"],
       datasets: [
         {
-          label: "# of Votes",
-          data: [10, 19, 3, 5, 2, 3],
+          label: "Cost vs Tank",
+          data: [23, 12, 6, 7, 12, 20, 22, 28],
           backgroundColor: [
             "rgba(255, 99, 132, 0.2)",
             "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
+            "rgba(255, 0, 0, 0.8);",
             "rgba(75, 192, 192, 0.2)",
             "rgba(153, 102, 255, 0.2)",
             "rgba(255, 159, 64, 0.2)"
@@ -19,7 +19,7 @@ window.executeHome = () => {
           borderColor: [
             "rgba(255,99,132,1)",
             "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
+            "rgba(255, 0, 0, 0.8);",
             "rgba(75, 192, 192, 1)",
             "rgba(153, 102, 255, 1)",
             "rgba(255, 159, 64, 1)"
@@ -34,6 +34,21 @@ window.executeHome = () => {
           {
             ticks: {
               beginAtZero: true
+            },
+            scaleLabel: {
+              display: true,
+              labelString: "Total Cost"
+            }
+          }
+        ],
+        xAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            },
+            scaleLabel: {
+              display: true,
+              labelString: "Number of Tanks"
             }
           }
         ]
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,87 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
 export default class Home extends Component {
   componentDidMount() {
     window.executeHome();
   }
   render() {
     return (
-      <div
-        style={{ marginLeft: "220px", marginTop: "60px", overflowX: "hidden" }}
-      >
-        <canvas id="myChart" width="400" height="200" />
-        <div className="row">
+      <div className="container-fluid">
+        <div className="row pt4">
           <div className="col">
-            <h1>Total Tanks:10</h1>
+            <h1
+              style={{
+                fontFamily: "Montserrat",
+                fontWeight: "bold",
+                fontSize: "300%"
+              }}
+            >
+              <u>RWHOptimizer</u>
+            </h1>
+          </div>
+        </div>
+        <div
+          style={{
+            marginLeft: "220px",
+            marginTop: "60px",
+            overflowX: "hidden"
+          }}
+          className="row"
+        >
+          <div className="col-6 pt4">
+            <h2 style={{ fontFamily: "Montserrat" }}>
+              <u>Build new model</u>
+            </h2>
+            <h6
+              style={{ fontFamily: "Montserrat", display: "inline-block" }}
+              className="pt4 pl6 pr2"
+            >
+              Select new file:
+            </h6>
+            <input type="file" name="file" id="" />
+            <h5
+              style={{ fontFamily: "Montserrat", textAlign: "center" }}
+              className="pt5 pr2"
+            >
+              <u>Model Summary:</u>
+            </h5>
+            <h6
+              style={{ fontFamily: "Montserrat", textAlign: "center" }}
+              className="pt4 pr2"
+            >
+              City : New Delhi
+            </h6>
+            <h6
+              style={{ fontFamily: "Montserrat", textAlign: "center" }}
+              className="pt4 pr2"
+            >
+              Active Nodes : 6
+            </h6>
+            <h5
+              style={{ fontFamily: "Montserrat", textAlign: "center" }}
+              className="pt5 pr2"
+            >
+              <u>Analysis:</u>
+            </h5>
+            <h6
+              style={{ fontFamily: "Montserrat", textAlign: "center" }}
+              className="pt4 pr2"
+            >
+              No of tanks : 6
+            </h6>
+            <h6
+              style={{ fontFamily: "Montserrat", textAlign: "center" }}
+              className="pt4 pr2"
+            />
+          </div>
+          <div
+            className="col-6"
+            style={{
+              paddingRight: "10%",
+              marginTop: "3%"
+            }}
+          >
+            <canvas id="myChart" width="500" height="400" />
           </div>
         </div>
       </div>
